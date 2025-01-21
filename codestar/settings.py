b3afa5548f26bbff1d907de3e552475b2a45f933
Jack Groves <jxckgrxves10@gmail.com>
2025-01-21 19:29:32 +0000

    fixed settings file for redeploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qtpyqt!s(gd1%@*$f3q!&(jde2i42#j^s2-g=(9l7ixb6ybf(d"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -85,12 +85,9 @@ WSGI_APPLICATION = "codestar.wsgi.application"
 #     }
 # }
 
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
